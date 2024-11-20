069633d412735f9544ac724f5516fe40fee3ad0b
David Cabiness <dcabines@gmail.com>
2024-11-20 15:47:14 -0500

    remove tech collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,13 @@
 import { z, defineCollection } from "astro:content";
 
-const experienceCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    position: z.string(),
-    location: z.string(),
-    dates: z.string(),
-  }),
-});
-
-const techCollection = defineCollection({
-    type: "content"
-  });
-
 export const collections = {
-  experience: experienceCollection,
-  tech: techCollection
+  experience: defineCollection({
+    type: "content",
+    schema: z.object({
+      company: z.string(),
+      position: z.string(),
+      location: z.string(),
+      dates: z.string(),
+    }),
+  }),
 };

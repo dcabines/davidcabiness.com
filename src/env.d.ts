b26f1c18d1968b76cf05d8f94010b6f5a6e5be85
David Cabiness <dcabines@gmail.com>
2024-11-21 21:56:47 -0500

    add kv

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
+
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+
+type ENV = {
+  // replace `MY_KV` with your KV namespace
+  MY_KV: KVNamespace;
+};
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+	interface Locals extends Runtime {}
+}
--- src/pages/api/api-test.ts
+++ src/pages/api/api-test.ts
@@ -1,10 +1,14 @@
 import type { APIContext } from "astro";
 
-export async function GET({ params, request }: APIContext) {
+export async function GET({ locals, params, request }: APIContext) {
+  const myKV = locals.runtime.env.MY_KV;
+  const value = await myKV.get("key");
+
   return new Response(
     JSON.stringify({
       name: "David Cabiness",
       url: "google.com",
+      value
     }),
   );
 }

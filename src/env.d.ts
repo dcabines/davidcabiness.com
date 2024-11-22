16e9b722dac48feb46e9c1d359fc137da6c22a2e
David Cabiness <dcabines@gmail.com>
2024-11-22 13:44:59 -0500

    fix types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../.astro/types.d.ts" />
+/// <reference path="astro/types.d.ts" />
 
 import type { D1Database, KVNamespace } from "@cloudflare/workers-types";
 import type { Runtime } from "@astrojs/cloudflare";
@@ -8,7 +8,6 @@ type ENV = {
   oltp: D1Database;
 };
 
-
 declare namespace App {
-	interface Locals extends Runtime<Env> {}
+  interface Locals extends Runtime<Env> {}
 }

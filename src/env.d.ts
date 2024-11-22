ddc437d174316fee66d1a2d25c3776d24f31b1f9
David Cabiness <dcabines@gmail.com>
2024-11-22 13:11:31 -0500

    add db

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 
+import type { D1Database } from "@cloudflare/workers-types";
+
 type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
 
 type ENV = {
   // replace `MY_KV` with your KV namespace
   MY_KV: KVNamespace;
+  oltp: D1Database;
 };
 
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
--- src/pages/api/api-test.ts
+++ src/pages/api/api-test.ts
@@ -4,11 +4,15 @@ export async function GET({ locals, params, request }: APIContext) {
   const myKV = locals.runtime.env.MY_KV;
   const value = await myKV.get("key");
 
+  const oltp = locals.runtime.env.oltp;
+  const todos = await oltp.prepare("select * from todo").all();
+
   return new Response(
     JSON.stringify({
       name: "David Cabiness",
       url: "google.com",
-      value
+      value,
+      todos
     }),
   );
 }

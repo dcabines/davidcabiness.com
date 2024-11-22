3f44b3b3a425ecd7504bd32701fe5377c1a02364
David Cabiness <dcabines@gmail.com>
2024-11-22 13:24:29 -0500

    update types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,17 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 
-import type { D1Database } from "@cloudflare/workers-types";
-
-type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+import type { D1Database, KVNamespace } from "@cloudflare/workers-types";
+import type { Runtime } from "@astrojs/cloudflare";
 
 type ENV = {
-  // replace `MY_KV` with your KV namespace
   MY_KV: KVNamespace;
   oltp: D1Database;
 };
 
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
-	interface Locals extends Runtime {}
+	interface Locals extends Runtime<Env> {}
 }

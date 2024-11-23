17c3037213cdf7e1a7ff1ec6b0a6c9271c9d4cc4
David Cabiness <dcabines@gmail.com>
2024-11-22 21:55:12 -0500

    make changes

--- src/todo/todo.svelte
+++ src/todo/todo.svelte
@@ -1,10 +1,19 @@
 <script>
   let { todos } = $props();
+
+  const onchange = (e, todo) => {
+    fetch("/api/todos/", {
+      method: "PUT",
+      body: JSON.stringify({
+        id: todo.id,
+        description: e.value
+      }),
+    });
+  };
 </script>
 
-
 <div>
-    {#each todos as todo}
-    <input value={todo.description} />
-    {/each}
+  {#each todos as todo}
+    <input value={todo.description} onchange={(e) => onchange(e, todo)} />
+  {/each}
 </div>

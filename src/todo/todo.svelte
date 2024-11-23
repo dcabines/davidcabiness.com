2b59e301bc26eee2e7102d2173320168031b12e8
David Cabiness <dcabines@gmail.com>
2024-11-23 13:00:48 -0500

    different shade of red

--- src/todo/todo.svelte
+++ src/todo/todo.svelte
@@ -79,7 +79,7 @@
           height="24"
           viewBox="0 0 24 24"
           fill="none"
-          stroke="red"
+          stroke="#ff6188"
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"

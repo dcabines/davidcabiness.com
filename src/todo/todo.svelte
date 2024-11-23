8844db5d45bcc07c8b027a0d484de5485d1d564b
David Cabiness <dcabines@gmail.com>
2024-11-22 21:50:18 -0500

    inputs

--- src/todo/todo.svelte
+++ src/todo/todo.svelte
@@ -1,34 +1,10 @@
 <script>
-	import { writable } from 'svelte/store';
-
-	const progress = writable(0.5);
+  let { todos } = $props();
 </script>
 
-<progress value={$progress}></progress>
 
-<button onclick={() => progress.set(0)}>
-	0%
-</button>
-
-<button onclick={() => progress.set(0.25)}>
-	25%
-</button>
-
-<button onclick={() => progress.set(0.5)}>
-	50%
-</button>
-
-<button onclick={() => progress.set(0.75)}>
-	75%
-</button>
-
-<button onclick={() => progress.set(1)}>
-	100%
-</button>
-
-<style>
-	progress {
-		display: block;
-		width: 100%;
-	}
-</style>
+<div>
+    {#each todos as todo}
+    <input value={todo.description} />
+    {/each}
+</div>

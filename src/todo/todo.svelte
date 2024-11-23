8cadeff1327d0978d5c24d8daadd9f529f8feee3
David Cabiness <dcabines@gmail.com>
2024-11-23 12:58:29 -0500

    inline svg

--- src/todo/todo.svelte
+++ src/todo/todo.svelte
@@ -73,7 +73,22 @@
         aria-label={`Delete ${todo.description}`}
         onclick={() => ondelete(todo)}
       >
-        <img src="/icons/trash.svg" alt={`Delete ${todo.description}`} />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="red"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polyline points="3 6 5 6 21 6"></polyline>
+          <path
+            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
+          ></path>
+        </svg>
       </button>
     </div>
   {/each}

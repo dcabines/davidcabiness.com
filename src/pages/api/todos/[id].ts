62d814c9d63b23cc90e227d6130b1b4399b5db78
David Cabiness <dcabines@gmail.com>
2024-11-22 20:59:17 -0500

    add put and delete

--- src/pages/api/todos/[id].ts
+++ src/pages/api/todos/[id].ts
@@ -2,12 +2,36 @@ import type { APIContext } from "astro";
 
 export async function GET({ locals, params }: APIContext) {
   const { oltp } = locals.runtime.env;
+  const sql = "select * from todo where id = ?1";
+  const todos = await oltp.prepare(sql).bind(params.id).all();
+  const todo = todos.results[0];
 
-  const todos = await oltp
-    .prepare("select * from todo where id = ?1")
-    .bind(params.id)
-    .all();
+  if (!todo) {
+    return new Response(null, { status: 404 });
+  }
 
+  const body = JSON.stringify(todos.results[0]);
+
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  return new Response(body, options);
+}
+
+export async function DELETE({ locals, params }: APIContext) {
+  const { oltp } = locals.runtime.env;
+  const sql = "delete from todo where id = ?1";
+  await oltp.prepare(sql).bind(params.id).all();
+}
+
+export async function PUT({ locals, params, request }: APIContext) {
+  const { oltp } = locals.runtime.env;
+  const { description } = await request.json();
+  const sql = "update todo set description = '?2' where id = ?1";
+  const todos = await oltp.prepare(sql).bind(params.id, description).all();
   const todo = todos.results[0];
 
   if (!todo) {

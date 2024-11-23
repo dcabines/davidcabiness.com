576957fd05aed157e50e55ee4177328da6b4f75b
David Cabiness <dcabines@gmail.com>
2024-11-22 21:09:16 -0500

    rearrange params

--- src/pages/api/todos/[id].ts
+++ src/pages/api/todos/[id].ts
@@ -31,6 +31,6 @@ export async function PUT({ locals, params, request }: APIContext) {
   const { oltp } = locals.runtime.env;
   const { description } = await request.json();
   console.log(description);
-  const sql = "update todo set description = '?2' where id = ?1";
-  await oltp.prepare(sql).bind(params.id, description).all();
+  const sql = "update todo set description = '?1' where id = ?2";
+  await oltp.prepare(sql).bind(description, params.id).all();
 }
